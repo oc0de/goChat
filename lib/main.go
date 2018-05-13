2c1ca62dfb2c56e5bb19973b10354ddd49a1d444
Ehsan <valizade@mail.gvsu.edu>
2018-05-12 23:50:03 -0500

    Add handle host

--- lib/main.go
+++ lib/main.go
@@ -24,12 +24,26 @@ func RunHost(ip string) {
 	}
 	fmt.Println("New connection accepted")
 
+	for {
+		handleHost(conn)
+	}
+}
+
+func handleHost(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 	message, readErr := reader.ReadString('\n')
 	if readErr != nil {
 		log.Fatal("Error: ", readErr)
 	}
 	fmt.Println("Message received: ", message)
+
+	fmt.Print("Send message: ")
+	replyReader := bufio.NewReader(os.Stdin)
+	replyMessage, replyErr := replyReader.ReadString('\n')
+	if replyErr != nil {
+		log.Fatal("Error: ", replyErr)
+	}
+	fmt.Fprint(conn, replyMessage)
 }
 
 // RunGuest takes destination ip
@@ -40,6 +54,7 @@ func RunGuest(ip string) {
 	if dialErr != nil {
 		log.Fatal("Error: ", dialErr)
 	}
+
 	fmt.Print("Send message: ")
 	reader := bufio.NewReader(os.Stdin)
 	message, readErr := reader.ReadString('\n')
