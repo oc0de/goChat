53b6d458dfb6422825fe273f81d12f93ee314fb3
Ehsan <valizade@mail.gvsu.edu>
2018-05-12 23:39:03 -0500

    Refactor and Add guest host

---
+++ lib/main.go
@@ -0,0 +1,50 @@
+package lib
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+const port = "8080"
+
+// RunHost takes an ip as argument
+func RunHost(ip string) {
+	ipAndPort := ip + ":" + port
+	listener, listenErr := net.Listen("tcp", ipAndPort)
+	if listenErr != nil {
+		log.Fatal("Error: ", listenErr)
+	}
+	fmt.Println("Listening on", ipAndPort)
+	conn, acceptErr := listener.Accept()
+	if acceptErr != nil {
+		log.Fatal("Error: ", acceptErr)
+	}
+	fmt.Println("New connection accepted")
+
+	reader := bufio.NewReader(conn)
+	message, readErr := reader.ReadString('\n')
+	if readErr != nil {
+		log.Fatal("Error: ", readErr)
+	}
+	fmt.Println("Message received: ", message)
+}
+
+// RunGuest takes destination ip
+func RunGuest(ip string) {
+	ipAndPort := ip + ":" + port
+	conn, dialErr := net.Dial("tcp", ipAndPort)
+
+	if dialErr != nil {
+		log.Fatal("Error: ", dialErr)
+	}
+	fmt.Print("Send message: ")
+	reader := bufio.NewReader(os.Stdin)
+	message, readErr := reader.ReadString('\n')
+	if readErr != nil {
+		log.Fatal("Error: ", readErr)
+	}
+	fmt.Fprint(conn, message)
+}
