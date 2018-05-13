ac66691fd0ac434fb683809e8e4559bb463fdabe
Ehsan <valizade@mail.gvsu.edu>
2018-05-12 19:40:32 -0500

    Add host part

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello")
+	var isHost bool
+	flag.BoolVar(&isHost, "listen", false, "Listens on the specified ip address")
+	flag.Parse()
+
+	if isHost {
+		// go run main.go -listen <ip>
+		connIP := os.Args[2]
+		runHost(connIP)
+	} else {
+		// go run main.go <ip>
+		connIP := os.Args[1]
+		runGuest(connIP)
+	}
+}
+
+const port = "8080"
+
+func runHost(ip string) {
+	ipAndPort := ip + ":" + port
+	listener, listenErr := net.Listen("tcp", ipAndPort)
+	if listenErr != nil {
+		log.Fatal("Error: ", listenErr)
+	}
+
+	conn, acceptErr := listener.Accept()
+	if acceptErr != nil {
+		log.Fatal("Error: ", acceptErr)
+	}
+
+	reader := bufio.NewReader(conn)
+	message, readErr := reader.ReadString('\n')
+	if readErr != nil {
+		log.Fatal("Error: ", readErr)
+	}
+	fmt.Println("Message received: ", message)
+}
+
+func runGuest(ip string) {
+
 }
